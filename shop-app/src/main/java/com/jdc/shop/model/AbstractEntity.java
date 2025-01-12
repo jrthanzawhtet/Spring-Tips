42454376456a5ac9762df105570050ddfc21c3b7
jrthanzawhtet <thanzawhtet18@gmail.com>
2025-01-12 23:04:53 +0630

    lazy sunday

---
+++ shop-app/src/main/java/com/jdc/shop/model/AbstractEntity.java
@@ -0,0 +1,16 @@
+package com.jdc.shop.model;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+@EntityListeners(value = AuditingEntityListener.class)
+public abstract class AbstractEntity {
+	
+	
+
+}
