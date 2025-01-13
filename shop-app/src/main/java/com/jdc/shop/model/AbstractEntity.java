96375a70cb5d4c95affb6312339dc213bba9352c
jrthanzawhtet <thanzawhtet18@gmail.com>
2025-01-13 07:21:47 +0630

    security update

--- shop-app/src/main/java/com/jdc/shop/model/AbstractEntity.java
+++ shop-app/src/main/java/com/jdc/shop/model/AbstractEntity.java
@@ -1,5 +1,11 @@
 package com.jdc.shop.model;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.EntityListeners;
@@ -11,6 +17,17 @@ import lombok.Data;
 @EntityListeners(value = AuditingEntityListener.class)
 public abstract class AbstractEntity {
 	
+	@CreatedBy
+	private String createBy;
+	
+	@CreatedDate
+	private LocalDateTime createAt;
+	
+	@LastModifiedBy
+	private String updateBy;
+	
+	@LastModifiedDate
+	private LocalDateTime updateAt;
 	
 
 }
